c7ee8a5e6a9c649a62ed9b2cb94deb6f5c783a84
cch5ng <cch5ng@gmail.com>
2014-11-19 15:01:09 -0800

    try to revert to older version where there is no condition on creating the random pizza elements

--- views/js/main.js
+++ views/js/main.js
@@ -520,12 +520,10 @@ else {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
-document.addEventListener('DOMContentLoaded', function() {
-  for (var i = 2; i < 100; i++) {
-    var pizzasDiv = document.getElementById("randomPizzas");
-    pizzasDiv.appendChild(pizzaElementGenerator(i));
-  }
-});
+for (var i = 2; i < 100; i++) {
+  var pizzasDiv = document.getElementById("randomPizzas");
+  pizzasDiv.appendChild(pizzaElementGenerator(i));
+}
 
 // User Timing API again. These measurements tell you how long it took to generate the initial pizzas
 window.performance.mark("mark_end_generating");
