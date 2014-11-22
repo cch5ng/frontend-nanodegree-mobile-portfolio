2692bac7ad95f7d061cda28b51e96c27c5cb1d93
cch5ng <cch5ng@gmail.com>
2014-11-22 12:54:56 -0800

    try enable srcset for pizzaImage with the new pizza png pointers. see if this helps for mobile download

--- views/js/main.js
+++ views/js/main.js
@@ -378,7 +378,7 @@ var pizzaElementGenerator = function(i) {
   pizzaContainer.id = "pizza" + i;                // gives each pizza element a unique id
   pizzaImageContainer.classList.add("col-md-6");
 
-  //pizzaImage.srcset = "images/pizza_lg.png 212w, images/pizza_med.png 108w"; //updated to try out responsive images
+  pizzaImage.srcset = "images/pizza.png 212w, images/pizza_lg_phn.png 67w"; //updated to try out responsive images
   pizzaImage.src = "images/pizza.png"; //updated to try out responsive images
   pizzaImage.classList.add("img-responsive");
   pizzaImageContainer.appendChild(pizzaImage);
