2dd9b128176c949e0621e902dd06d1eeaf05d419
cch5ng <cch5ng@gmail.com>
2014-11-25 13:32:50 -0800

    try tshoot value of var ticking. this seems to be reason why bgd pizzas do not move on scroll

--- views/js/main.js
+++ views/js/main.js
@@ -552,12 +552,10 @@ function updatePositions() {
 
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    if (!ticking) {
-      console.log('items[' + i + '].scrollTop: ' + items[i].scrollTop);
-      console.log('latestKnownScrollY: ' + latestKnownScrollY);
-      var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-      items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
-    }
+    console.log('items[' + i + '].scrollTop: ' + items[i].scrollTop);
+    console.log('latestKnownScrollY: ' + latestKnownScrollY);
+    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
