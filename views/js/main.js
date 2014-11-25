5fabd19751f50004f0e0b97e000eb8096e34a892
cch5ng <cch5ng@gmail.com>
2014-11-25 00:31:55 -0800

    debug background pizzas. 8 pizzas overlaid in same col per row

--- views/js/main.js
+++ views/js/main.js
@@ -579,6 +579,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
+    console.log(elem.basicLeft);
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
