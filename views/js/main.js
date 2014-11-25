ba7bea1bc82c9940a861855ca2e4bff6c8154563
cch5ng <cch5ng@gmail.com>
2014-11-25 14:21:55 -0800

    try improve perf by adding condition that new phase only calculated and applied when the item is in view

--- views/js/main.js
+++ views/js/main.js
@@ -529,14 +529,12 @@ var ticking = false,
 
 function onScroll() {
   latestKnownScrollY = window.scrollY;
-  //console.log('latestKnownScrollY: ' + latestKnownScrollY);
   requestTick();
 }
 
 function requestTick() {
   if (!ticking) {
     requestAnimationFrame(updatePositions);
-    console.log('updatePositions got called onscroll');
   }
   ticking = true;
 }
@@ -544,23 +542,16 @@ function requestTick() {
 // Moves the sliding background pizzas based on scroll position
 function updatePositions() {
   var currentScrollY = latestKnownScrollY;
-  //console.log('currentScrollY: ' + currentScrollY);
-  //if (currentScrollY > 0) {
   ticking = false;
-  //}
-  //console.log('ticking: ' + ticking);
   frame++;
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    //console.log('items[' + i + '].scrollTop: ' + items[i].scrollTop);
-    //console.log('latestKnownScrollY: ' + latestKnownScrollY);
-    var phase = Math.sin(( currentScrollY/ 1250) + (i % 5)); //document.body.scrollTop
-    //console.log('document.body.scrollTop: ' + document.body.scrollTop);
-    console.log('phase: ' + phase);
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
-    console.log('items[i].style.left: ' + items[i].style.left);
+    if (items[i].scrollTop >= currentScrollY) {
+      var phase = Math.sin(( currentScrollY/ 1250) + (i % 5)); //document.body.scrollTop
+      items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    }
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
