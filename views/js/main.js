e2e938f65d42537be2e560e6bdc6a42737c6d027
cch5ng <cch5ng@gmail.com>
2014-11-25 00:58:50 -0800

    fix DOMContentLoaded listener

--- views/js/main.js
+++ views/js/main.js
@@ -583,6 +583,6 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
-  requestTick();
-  //updatePositions();
+  //requestTick();
+  updatePositions();
 });
