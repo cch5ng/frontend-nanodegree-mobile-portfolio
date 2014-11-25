f2b5d5240b25bd3cb3b0e9261553fc8c624a7220
cch5ng <cch5ng@gmail.com>
2014-11-25 00:36:20 -0800

    made latestKnownScrollY global var so it can be used by DOMContentLoaded event listener

--- views/js/main.js
+++ views/js/main.js
@@ -524,10 +524,10 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // The following code for sliding background pizzas was pulled from Ilya's demo found at:
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
-var ticking = false;
+var ticking = false,
+  latestKnownScrollY = window.scrollY;
 
 function onScroll() {
-  latestKnownScrollY = window.scrollY;
   requestTick();
 }
 
