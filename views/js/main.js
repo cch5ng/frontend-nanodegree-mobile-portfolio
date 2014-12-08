6e76dd3b8adf56a3cbeeef70759bfa8a726d1b1a
cch5ng <cch5ng@gmail.com>
2014-12-08 13:24:52 -0800

    moved logic for drawing bgd pizzas and copy of updatePosition() to hmtl. made link to main.js async

--- views/js/main.js
+++ views/js/main.js
@@ -395,6 +395,7 @@ var pizzaElementGenerator = function(i) {
 
   //pizzaImage.srcset = "images/pizza.png 212w, images/pizza_lg_phn.png 67w"; //updated to try out responsive images
   pizzaImage.src = "images/pizza.png"; //updated to try out responsive images
+  pizzaImage.alt = "pizza";
   pizzaImage.classList.add("img-responsive");
   //pizzaImageContainer.appendChild(pizzaImage);
   pizzaPicture.appendChild(pizzaImage);
@@ -515,9 +516,9 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
 var ticking = false,
-  ticking2 = false,
   latestKnownScrollY = 0,
-  firstRandomPizzasDrawn = false;
+  //firstRandomPizzasDrawn = false,
+  lastRandomPizzaDrawn = false;
 
 //REV NOTE - 1 separating scroll event from the repaint event for background pizzas; 2 defering generation of random pizzas (foreground) to the 
 //first scroll event => this should speed up initial page load for contents above the fold
@@ -525,10 +526,8 @@ function onScroll() {
   latestKnownScrollY = window.scrollY;
   //console.log('latestKnownScrollY: ' + latestKnownScrollY);
   requestTick();
-  if (!firstRandomPizzasDrawn) {
+  if (!lastRandomPizzaDrawn) {
     drawRandomPizzas();
-  } else {
-    requestTick2();
   }
 }
 
@@ -537,11 +536,14 @@ function drawRandomPizzas() {
 
   // This for-loop actually creates and appends all of the pizzas when the page loads
   //for (var i = 2; i < 100; i++) { //original
-  for (var i = 2; i < 20; i++) { //trying to render only what would show when the pizza slider control displays
+  for (var i = 0; i < 33; i++) { //trying to render only what would show when the pizza slider control displays
     var pizzasDiv = document.getElementById("randomPizzas");
     pizzasDiv.appendChild(pizzaElementGenerator(i));
   }
-  firstRandomPizzasDrawn = true;
+  if (document.querySelectorAll(".randomPizzaContainer").length >= 100) {
+    lastRandomPizzaDrawn = true;
+  }
+  //firstRandomPizzasDrawn = true;
 
   // User Timing API again. These measurements tell you how long it took to generate the initial pizzas
   window.performance.mark("mark_end_generating");
@@ -558,15 +560,7 @@ function requestTick() {
   ticking = true;
 }
 
-function requestTick2() {
-  if (!ticking2) {
-    requestAnimationFrame(drawRemainingRandomPizzas);
-    console.log('called drawRemainingRandomPizzas');
-  }
-  ticking2 = true;
-}
-
-// Moves the sliding background pizzas based on scroll position
+  // Moves the sliding background pizzas based on scroll position
 function updatePositions() {
   var currentScrollY = latestKnownScrollY;
   ticking = false;
@@ -589,53 +583,5 @@ function updatePositions() {
   }
 }
 
-function drawRemainingRandomPizzas() {
-  var currentScrollY = latestKnownScrollY;
-  console.log('currentScrollY: ' + currentScrollY);
-  var numRandomPizzas = document.querySelectorAll(".randomPizzaContainer").length;
-  ticking2 = false;
-  if (numRandomPizzas < 100) {
-    //TODO this is test to check how many random pizzas need to display in the current screen view
-    //console.log('top of the last random pizza: ' + document.querySelectorAll(".randomPizzaContainer")[numRandomPizzas - 1].scrollTop);
-    console.log('scrnHeight: ' + scrnHeight);
-    console.log('numRandomPizzas: ' + numRandomPizzas);
-
-    if (currentScrollY > (scrnHeight - numRandomPizzas / 4 * 280)) { //rough estimate for min row height based on 3 pizza sizes
-      for (var i = 0; i < 16; i++) {
-        var pizzasDiv = document.getElementById("randomPizzas");
-        pizzasDiv.appendChild(pizzaElementGenerator(i));
-      }
-      //console.log('top of the last random pizza: ' + document.querySelectorAll(".randomPizzaContainer")[numRandomPizzas - 1].scrollTop);
-    }
-  }
-}
-
 // runs updatePositions on scroll
 window.addEventListener('scroll', onScroll);
-
-// Generates the sliding pizzas when the page loads.
-document.addEventListener('DOMContentLoaded', function() {
-  scrnHeight = window.screen.height;
-  var cols = 8;
-  //console.log('window.screen.width: ' + window.screen.width);
-  var screenWidth = window.screen.width;
-  var s = screenWidth / 6;
-  //var imageCorrection = 1 / window.screen.width;
-  //var imageWidthCorrection = 73.333 / window.screen.width;
-  //var s = 256;
-
-  //for (var i = 0; i < 200; i++) { //original count of background pizzas generated onload
-  for (var i = 0; i < 88; i++) { //REV NOTE decreased initial count of bgd pizzas generated onload
-    var elem = document.createElement('img');
-    elem.className = 'mover';
-    elem.src = "images/pizza.png";
-//REV NOTE set height, width using existing mover class in CSS
-    //elem.style.height = (screenWidth / 12).toString() + 'px'; //14.4
-    //elem.style.width = (screenWidth / 16.44).toString() + 'px'; //19.9
-    elem.basicLeft = (i % cols) * s;
-    elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
-  }
-  //requestTick();
-  updatePositions();
-});
