3bf56396ae64eb60f878e6d61084a251cdd72565
cch5ng <cch5ng@gmail.com>
2014-12-13 22:37:19 -0800

    moved the DOMContentLoaded event listener (generate bgd pizzas) to html file. copied a simpler version of updatePositions() into that event listener. made the link to main.js as async

--- views/js/main.js
+++ views/js/main.js
@@ -529,7 +529,7 @@ function updatePositions() {
 
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((currentyScrollY / 1250) + (i % 5));
+    var phase = Math.sin((currentScrollY / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
@@ -547,18 +547,18 @@ function updatePositions() {
 window.addEventListener('scroll', onScroll);
 
 // Generates the sliding pizzas when the page loads.
-document.addEventListener('DOMContentLoaded', function() {
-  var cols = 6; // REV NOTE decreased num
-  var s = 256;
-  for (var i = 0; i < 36; i++) { // REV NOTE decreased num of background pizzas
-    var elem = document.createElement('img');
-    elem.className = 'mover';
-    elem.src = "images/pizza.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
-    elem.basicLeft = (i % cols) * s;
-    elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
-  }
-  updatePositions();
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//   var cols = 6; // REV NOTE decreased num
+//   var s = 256;
+//   for (var i = 0; i < 36; i++) { // REV NOTE decreased num of background pizzas
+//     var elem = document.createElement('img');
+//     elem.className = 'mover';
+//     elem.src = "images/pizza.png";
+//     elem.style.height = "100px";
+//     elem.style.width = "73.333px";
+//     elem.basicLeft = (i % cols) * s;
+//     elem.style.top = (Math.floor(i / cols) * s) + 'px';
+//     document.querySelector("#movingPizzas1").appendChild(elem);
+//   }
+//   updatePositions();
+// });
